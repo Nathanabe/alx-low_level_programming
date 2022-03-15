d5a60fbc2beef58ed4cb9a29d71cfd6e872d3157
nathanabe <nathanieladehor4@gmail.com>
2022-03-14 19:12:12 -0700

    fourth C task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#inclucde <stdio.h>
+#include <stdio.h>
 /**
  *main - entry point
  *
@@ -7,5 +7,5 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0):
+return (0);
 }
