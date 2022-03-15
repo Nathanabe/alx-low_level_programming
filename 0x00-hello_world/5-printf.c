ac1c1c126ce8e6477b255c0edd4e7b4c2fd93b74
nathanabe <nathanieladehor4@gmail.com>
2022-03-14 20:17:26 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#inclucde <stdio.h>
-/**
- *main - entry point
- *my hello, printf task
- *Return: return 1 after program execution
- */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
+#include <studio.h>                                                                                                                                      
+/**                                                                                                                                                      
+ *main - Entry point                                                                                                                                     
+ *my hello, printif task                                                                                                                                 
+ *Return: return 1 after program execution                                                                                                               
+ */                                                                                                                                                      
+int main(void)                                                                                                                                           
+{                                                                                                                                                        
+printf("with proper grammar, but the outcome is a piece of art,\n");                                                                                     
 return (0);
 }
