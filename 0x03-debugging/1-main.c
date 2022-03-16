587d3ad6de6d860c0aa4e7c19b80e11c77583fa4
nathanabe <nathanieladehor4@gmail.com>
2022-03-16 16:44:01 -0700

    my first 1-main.c commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - causes of infinite loop
+ * main - causes an infinite loop
  *
  * Return: 0
  */
@@ -9,11 +9,12 @@ int main(void)
 int i;
 
 printf("infinite loop incoming :(\n");
+
 i = 0;
 
 /*
+ *while (i<10)
  *{
- *while (i <10)
  *infinite loop - No increase of variable
  *putchar(i);
 }
