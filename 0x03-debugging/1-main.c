f63e0dca575c5e770ed7ce419d184b5ca72db0ab
nathanabe <nathanieladehor4@gmail.com>
2022-03-16 16:20:27 -0700

    nain c commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,14 +12,12 @@ printf("infinite loop incoming :(\n");
 
 i = 0;
 
-
 /*
  *while (i <10)
  *infinite loop - No increase of variable
  *putchar(i);
 }
 */
-
 printf("infinite loop avoided! \\o/\n");
 return (0);
 }
