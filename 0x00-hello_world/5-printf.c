77cf433e76b3e2c5e5c1adbb75783fc9af5d4eec
nathanabe <nathanieladehor4@gmail.com>
2022-03-17 06:57:39 -0700

    my 5-printf.c task commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *My hello. print task
+ * Return: return 1 after programm execution
+ */
+int main(void)
+{
+printif("\"with proper grammer. but the outcome is a piece of art.\n");
+return (0);
+}
