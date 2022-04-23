009475543924ac06c91cf2ec80ca5c1eec79d6a1
Nathanabe <99434652+Nathanabe@users.noreply.github.com>
2022-04-23 09:08:07 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-/** Writing a function that prints all the elements of a list_t list.
+/** Writing a function that prints all the elements of a list_t list./**
  * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
  * Return: number of elements in the list.
