734b8cd6553b8bfebb0175a3eab8fab2a25cf01f
Nathanabe <99434652+Nathanabe@users.noreply.github.com>
2022-05-02 23:02:07 +0100

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,39 @@
+#include <stdio.h>
 #include "main.h"
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
- * create_file - creates a file
- * @filename: the name of the file to create
- * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * create_file - Creates a file.
+ * standard output.
+ * @filename: Name of the file to create.
+ * @text_content: NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file, fwrite, i;
+	int fd, lenght;
+	ssize_t res_write;
 
-    if (filename == NULL)
-        return (-1);
-    file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-    if (file == -1)
-        return (-1);
-
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i]; i++)
-            ;
-        fwrite = write(file, text_content, i);
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		lenght = 0;
+		while (*(text_content + lenght) != '\0')
+			lenght++;
+		res_write = write(fd, text_content, lenght);
+		if (res_write == -1)
+		{
+			write(1, "fails", 6);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
